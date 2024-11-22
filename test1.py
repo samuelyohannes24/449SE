aa434407bf238620c010b2582099d2ea319997c6
samuelyohannes24 <58994918+samuelyohannes24@users.noreply.github.com>
2024-11-21 21:42:02 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,8 @@
+
 import unittest 
 import tkinter as tk
-from Sprint3 import GameLogic
-from Sprint3 import SOSGame
+from Sprint4 import GameLogic
+from Sprint4 import SOSGame
 
 
 # Reference: https://stackoverflow.com/questions/6854658/explain-the-setup-and-teardown-python-methods-used-in-test-cases
@@ -13,26 +14,21 @@ class TestGameLogic(unittest.TestCase):
         self.game_logic = SOSGame(self.root)
 
 
-    # Reference: https://stackoverflow.com/questions/6854658/explain-the-setup-and-teardown-python-methods-used-in-test-cases
-
-    def tearDown(self):
-
-        self.root.destroy()
-
+    
     def test_simple_mode_win(self):
-
         self.game_logic.mode.set('Simple')
+
+        self.game_logic.current_letter.set('S')
         self.game_logic.make_move(0, 0)
-        self.game_logic.current_letter.set('O')
-        self.game_logic.make_move(0, 1)
         self.game_logic.current_letter.set('S')
         self.game_logic.make_move(0, 2)
 
+        self.game_logic.current_letter.set('O')
+        self.game_logic.make_move(0, 1)
+
 
         self.assertEqual(self.game_logic.current_player, 'Blue')
-        self.assertEqual(self.game_logic.check_for_sos,(0, 2, 'S'))
 
-    
     
 
 
@@ -42,35 +38,78 @@ class TestGameLogic(unittest.TestCase):
         result = self.game_logic.make_move(0, 0)
         self.assertFalse(result)
 
+    
+    def test_CPUblue(self):
 
-    def test_general_mode_tie(self):
-
-        self.game_logic.mode.set('General')
         self.game_logic.board_size.set(3)
+        self.game_logic.current_blue_player.set("C")
         self.game_logic.create_board()
 
+        unvacant = 0
+        for i in range(self.game_logic.board_size.get()):
+            for j in range(self.game_logic.board_size.get()):
+                if self.game_logic.game_logic.board[i][j] != None:
+                    unvacant += 1
+        self.assertTrue(unvacant == 1)
 
-        moves = [
-            ('S', 0, 0), ('O', 0, 1), ('S', 0, 2),
-            ('S', 1, 0), ('O', 1, 1), ('S', 1, 2),
-            ('S', 2, 0), ('O', 2, 1), ('S', 2, 2),
+    def test_CPUred(self):
 
-        ]
+        self.game_logic.board_size.set(3)
+        self.game_logic.current_red_player.set("C")
+        self.game_logic.create_board()
 
-        for letter, row, col in moves:
-            self.game_logic.current_letter.set(letter)
-            self.game_logic.make_move(row, col)
+        self.game_logic.make_move(0, 0)
+        unvacant = 0
+        for i in range(self.game_logic.board_size.get()):
+            for j in range(self.game_logic.board_size.get()):
+                if self.game_logic.game_logic.board[i][j] != None:
+                    unvacant += 1
+        self.assertTrue(unvacant == 2)
 
-        self.assertTrue(self.game_logic.all_cells_filled())
-        self.assertEqual(self.game_logic.blue_score, self.game_logic.red_score)
+    def CPUblue_red_simple(self):
+    
+        self.game_logic.board_size.set(3)
+        self.current_blue_player.set("C")
+        self.current_red_player.set("C")
+        self.game_logic.create_board()
+        unvacant = 0
+        for i in range(self.game_logic.board_size.get()):
+            for j in range(self.game_logic.board_size.get()):
+                if self.game_logic.game_logic.board[i][j] != None:
+                    unvacant += 1
+        self.assertTrue(self.game_logic.game_logic.all_cells_filled())
 
-class SOSGame:
+    def CPUblue_red_general(self):
+    
+        self.game_logic.board_size.set(3)
+        self.current_blue_player.set("C")
+        self.current_red_player.set("C")
+        self.game_logic.create_board()
+        unvacant = 0
+        for i in range(self.game_logic.board_size.get()):
+            for j in range(self.game_logic.board_size.get()):
+                if self.game_logic.board[i][j] != None:
+                    unvacant += 1
+        self.assertTrue(unvacant == 9)
 
-    def all_cells_filled(self):
-        for row in self.board:
-            if any(cell == '' for cell in row):
-                return False
-            return True
+    def CPUblue_red_general(self):
+    
+        self.game_logic.board_size.set(3)
+        self.current_blue_player.set("C")
+        self.current_red_player.set("C")
+        self.game_logic.create_board()
+        unvacant = 0
+        for i in range(self.game_logic.board_size.get()):
+            for j in range(self.game_logic.board_size.get()):
+                if self.game_logic.board[i][j] != None:
+                    unvacant += 1
+        self.assertTrue(unvacant == 9)
+
+    # Reference: https://stackoverflow.com/questions/6854658/explain-the-setup-and-teardown-python-methods-used-in-test-cases
+
+    def tearDown(self):
+
+        self.root.destroy()
 
 if __name__ == '__main__':
     unittest.main()
